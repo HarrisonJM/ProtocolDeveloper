68c325d50ee69de81684a53e558686bd1903271b
HarrisonJM <harrison2.marcks@live.uwe.ac.uk>
2018-02-13 22:03:59 +0000

    PD-10 Created Base of helloWorld protocol and it's required accompanying server

--- Protocol/HelloWorldProtocol/HelloWorldProtocol.cpp
+++ Protocol/HelloWorldProtocol/HelloWorldProtocol.cpp
@@ -1,4 +1,27 @@
-//
-// Created by hmarcks on 10/02/18.
-//
+#include <string>
 
+#include "ProtocolInterface.h"
+/*
+ * This group of files will serve to act as a simple hello/goodby world protocol
+ * The protocl willbe used as a test bed for various functions of the protdev program
+ * It's first function will be simple to receive and send traffic
+ * After that it will be extended to have and process error codes
+ * It can then be extended to function with/as a sister protocol
+ *
+ */
+
+class HelloWorldProtocol : public ProtocolInterface
+{
+public:
+    HelloWorldProtocol();
+    ~HelloWorldProtocol();
+
+
+    void* resultReceived(void* payLoad) override;
+    void* dataToSend(void* payLoad) override;
+
+    // This method will be used to decode any received results
+    std::string decodeResult(void* payLoad);
+    // This method will be used to encode any data that needs to eb sent out
+    std::string encodeData(void* payLoad);
+};
---
+++ Protocol/HelloWorldProtocol/HelloWorldServer/HelloWorldServerMain.cpp
@@ -0,0 +1,10 @@
+
+/*
+ * A simple Hello World server. Will send Hello/goodbye world depending on what it receives.
+ * Will be used to help develop and test different features of the prot dev program
+ */
+
+int main(int argv, char** argc)
+{
+
+}
