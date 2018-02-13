1821748da70973a23a411a631986e2d79608a628
Harrison James Marcks <harrison2.marcks@live.uwe.ac.uk>
2018-02-13 22:42:49 +0000

    PD-9 CommsHandler Skeleton. Attempting to fix CI

---
+++ CommunicationHandler/CommunicationHandler.h
@@ -0,0 +1,20 @@
+//
+// Created by hmarcks on 07/02/18.
+//
+
+#ifndef PROTOCOLDEVELOPER_COMMUNICATIONHANDLER_H
+#define PROTOCOLDEVELOPER_COMMUNICATIONHANDLER_H
+
+namespace CommunicationHandler
+{
+    class CommunicationHandler
+    {
+    public:
+        CommunicationHandler();
+        ~CommunicationHandler();
+
+    private:
+    };
+}
+
+#endif //PROTOCOLDEVELOPER_COMMUNICATIONHANDLER_H
