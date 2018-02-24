620445e213f46a19138b1d7c0a78f42107666c3c
Harrison James Marcks <harrison2.marcks@live.uwe.ac.uk>
2018-02-24 22:36:37 +0000

    PD-15 should be fixed.

--- configureBuild.sh
+++ configureBuild.sh
@@ -4,10 +4,10 @@ export CC=$(which cc)
 export CXX=$(which g++)
 
 if [[ ! -e "build/" ]]; then
-    mkdir ../build
+    mkdir build
 fi
 
-cd ../build
+cd build
 
 # cmake will automagically pick up the CMakeFileLists.txt and commence the build
 # where it was called
